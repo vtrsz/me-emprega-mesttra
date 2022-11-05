976a1ff8fbfbff18444eddb5b329f56cc25e2aae
vtrsz <vitorsouzaribeiro1@gmail.com>
2022-11-05 15:08:26 -0300

    feat: Spring Structure

---
+++ src/main/java/com/mesttra/meemprega/MeEmpregaApplication.java
@@ -0,0 +1,13 @@
+package com.mesttra.meemprega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeEmpregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeEmpregaApplication.class, args);
+	}
+
+}
